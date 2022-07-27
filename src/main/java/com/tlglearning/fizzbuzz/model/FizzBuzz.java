3a92186b6262804845e27bb679e1af9c134a7b33
nabindhakall <nabindhakall@hotmail.com>
2022-07-27 09:53:17 -0700

    Initial commit

---
+++ src/main/java/com/tlglearning/fizzbuzz/model/FizzBuzz.java
@@ -0,0 +1,33 @@
+package com.tlglearning.fizzbuzz.model;
+
+import com.tlglearning.fizzbuzz.model.Analysis;
+import com.tlglearning.fizzbuzz.model.State;
+import java.util.Set;
+import org.apache.commons.cli.Options;
+
+public class FizzBuzz {
+
+  private static final int DEFAULT_UPPER_BOUND = 100;
+
+  public static void main(String[] args) {
+    try {
+      int upperBound = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_UPPER_BOUND;
+      if (upperBound <= 0){
+        throw new IllegalAccessException();
+      }
+      Analysis analysis = new Analysis();
+      for (int counter = 1; counter <=upperBound; counter++) {
+        Set<State> result = analysis.analyze(counter);
+        System.out.println(result.isEmpty() ? counter : result);
+
+        }
+    } catch (IllegalAccessException e) {
+      System.out.printf("Usage: java %1$s [upperBound]%n", FizzBuzz.class.getName());
+      System.out.println("Where: upperBound is a positive integer, with a default of 100");
+
+    } finally {
+      System.out.println("Thanks for playing FizBuzz!");
+    }
+  }
+
+  }
